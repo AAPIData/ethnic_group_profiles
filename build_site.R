615aecdd0fff0aabd878d95f9de4e0ab237a578a
Sono <sonoshah@gmail.com>
2017-10-03 20:52:49 -0700

    upload

---
+++ build_site.R
@@ -0,0 +1,33 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("~/Documents/aapidata_group_profiles/")
+#render your sweet site. 
+rmarkdown::render_site()
+
+setwd("~/Documenterers/aapidata_group_profiles/")
+groups = c("Asian Indian","Bangladeshi","Bhutanese","Burmese","Cambodian",
+           "Chinese",
+           "Filipino",
+           "Hmong",
+           "Indonesian",
+           "Japanese",
+           "Korean",
+           "Laotian",
+           "Malaysian",
+           "Mongolian",
+           "Nepalese",
+           "Okinawan",
+           "Pakistani",
+           "Sri Lankan",
+           "Taiwanese",
+           "Thai",
+           "Vietnamese")
+for(group in groups)
+  rmarkdown::render(input = "report_template.Rmd", 
+                    output_file = paste0(group,".html"),
+                    params = list(group = group)
+  )
+
+
